1a7fab7433d5b1900fdc8819df1763ab8b428b82
xsandre-l <ahloubser12@gmail.com>
2025-10-17 15:19:10 +0200

    .

--- .codeframe/package.ts
+++ .codeframe/package.ts
@@ -1,5 +1,6 @@
 import {
   BuildArchitectures,
+  BuildType,
   OUTPUT_DIR,
 } from "../../../src/types/package-config.ts";
 import { runPackageAction } from "../../../src/packages.ts";
@@ -7,7 +8,7 @@ import { runPackageAction } from "../../../src/packages.ts";
 import { resolve, join } from "node:path";
 import { argv } from "node:process";
 
-export const builds = (cwd: string = process.cwd()): BuildArchitectures => {
+export const build = (cwd: string = process.cwd()): BuildType => {
   const toolchain = resolve(cwd, "../../toolchains/llvm-mingw");
   const CLANG = join(toolchain, "bin/clang.exe").replace(/\\/g, "/");
   const CLANGXX = join(toolchain, "bin/clang++.exe").replace(/\\/g, "/");
@@ -18,8 +19,9 @@ export const builds = (cwd: string = process.cwd()): BuildArchitectures => {
   ).replace(/\\/g, "/");
 
   return {
+    type: "architectures",
     windows_x86_64: {
-      configStep: `cmake -S . -B build/build-x86_64 -G Ninja \
+      configStep: `cmake -S . -B build/windows/x86_64 -G Ninja \
       -DCMAKE_BUILD_TYPE=Release \
       -DBUILD_SHARED_LIBS=OFF \
       -DCMAKE_C_COMPILER=${CLANG} \
@@ -29,14 +31,14 @@ export const builds = (cwd: string = process.cwd()): BuildArchitectures => {
       -DCMAKE_CXX_COMPILER_TARGET=x86_64-w64-windows-gnu \
       -DCMAKE_SYSTEM_NAME=Windows \
       -DCMAKE_SYSTEM_PROCESSOR=x86_64 \
-      -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR}/windows/x86_64/glfw
+      -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR}/glfw/windows/x86_64
     `,
 
-      buildStep: `cmake --build build/build-x86_64 -j`,
-      installStep: `cmake --install build/build-x86_64`,
+      buildStep: `cmake --build build/windows/x86_64 -j`,
+      installStep: `cmake --install build/windows/x86_64`,
     },
     windows_aarch64: {
-      configStep: `cmake -S . -B build/build-aarch64 -G Ninja \
+      configStep: `cmake -S . -B build/windows/aarch64 -G Ninja \
       -DCMAKE_BUILD_TYPE=Release \
       -DBUILD_SHARED_LIBS=OFF \
       -DCMAKE_C_COMPILER=${CLANG} \
@@ -47,15 +49,25 @@ export const builds = (cwd: string = process.cwd()): BuildArchitectures => {
       -DCMAKE_CXX_COMPILER_TARGET=aarch64-w64-windows-gnu \
       -DCMAKE_SYSTEM_NAME=Windows \
       -DCMAKE_SYSTEM_PROCESSOR=aarch64 \
-      -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR}/windows/aarch64/glfw
+      -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR}/glfw/windows/aarch64
       `,
-      buildStep: `cmake --build build/build-aarch64 -j`,
-      installStep: `cmake --install build/build-aarch64`,
+      buildStep: `cmake --build build/windows/aarch64 -j`,
+      installStep: `cmake --install build/windows/aarch64`,
     },
-  } satisfies BuildArchitectures;
+    linux_x86_64: {
+      configStep: ``,
+      buildStep: ``,
+      installStep: ``,
+    },
+    linux_aarch64: {
+      configStep: ``,
+      buildStep: ``,
+      installStep: ``,
+    },
+  } satisfies BuildType;
 };
 
 const args = argv.slice(2);
 const [action = "help"] = args;
 
-await runPackageAction(action, process.cwd(), builds());
+await runPackageAction(action, process.cwd(), build());

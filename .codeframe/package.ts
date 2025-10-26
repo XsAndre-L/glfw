2eb4a89f5acd16abaa6a3deacd24a105ac412a50
xsandre-l <ahloubser12@gmail.com>
2025-10-26 10:41:36 +0200

    .

--- .codeframe/package.ts
+++ .codeframe/package.ts
@@ -1,14 +1,11 @@
-import {
-  BuildArchitectures,
-  BuildType,
-  OUTPUT_DIR,
-} from "../../../src/types/package-config.ts";
+import { BuildType, OUTPUT_DIR } from "../../../src/types/package-config.ts";
 import { runPackageAction } from "../../../src/packages.ts";
 
 import { resolve, join } from "node:path";
 import { argv } from "node:process";
 
 export const build = (cwd: string = process.cwd()): BuildType => {
+  const LINUX = resolve(cwd, "../../toolchains/linux");
   const toolchain = resolve(cwd, "../../toolchains/llvm-mingw");
   const CLANG = join(toolchain, "bin/clang.exe").replace(/\\/g, "/");
   const CLANGXX = join(toolchain, "bin/clang++.exe").replace(/\\/g, "/");
@@ -55,14 +52,38 @@ export const build = (cwd: string = process.cwd()): BuildType => {
       installStep: `cmake --install build/windows/aarch64`,
     },
     linux_x86_64: {
-      configStep: ``,
-      buildStep: ``,
-      installStep: ``,
+      configStep: `cmake -S . -B build/linux/x86_64 -G Ninja \
+      -DCMAKE_TOOLCHAIN_FILE=${LINUX}/linux_x86-64.cmake \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DBUILD_SHARED_LIBS=OFF \
+      -DGLFW_BUILD_WAYLAND=OFF \
+      -DCMAKE_C_COMPILER=${CLANG} \
+      -DCMAKE_CXX_COMPILER=${CLANGXX} \
+      -DX11_X11_INCLUDE_PATH=${LINUX}/linux22-sysroot/usr/include \
+      -DX11_X11_LIB=${LINUX}/linux22-sysroot/usr/lib/x86_64-linux-gnu/libX11.so \
+      -DCMAKE_C_COMPILER_TARGET=x86_64-unknown-linux-gnu \
+      -DCMAKE_CXX_COMPILER_TARGET=x86_64-unknown-linux-gnu \
+      -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR}/glfw/linux/x86_64
+      `,
+      buildStep: `cmake --build build/linux/x86_64 -j`,
+      installStep: `cmake --install build/linux/x86_64`,
     },
     linux_aarch64: {
-      configStep: ``,
-      buildStep: ``,
-      installStep: ``,
+      configStep: `cmake -S . -B build/linux/aarch64 -G Ninja \
+      -DCMAKE_TOOLCHAIN_FILE=${LINUX}/linux_aarch64.cmake \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DBUILD_SHARED_LIBS=OFF \
+      -DGLFW_BUILD_WAYLAND=OFF \
+      -DCMAKE_C_COMPILER=${CLANG} \
+      -DCMAKE_CXX_COMPILER=${CLANGXX} \
+      -DX11_X11_INCLUDE_PATH=${LINUX}/linux22-sysroot/usr/include \
+      -DX11_X11_LIB=${LINUX}/linux22-sysroot/usr/lib/aarch64-linux-gnu/libX11.so \
+      -DCMAKE_C_COMPILER_TARGET=aarch64-unknown-linux-gnu \
+      -DCMAKE_CXX_COMPILER_TARGET=aarch64-unknown-linux-gnu \
+      -DCMAKE_INSTALL_PREFIX=${OUTPUT_DIR}/glfw/linux/aarch64
+      `,
+      buildStep: `cmake --build build/linux/aarch64 -j`,
+      installStep: `cmake --install build/linux/aarch64`,
     },
   } satisfies BuildType;
 };
